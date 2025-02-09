0a39a01b35b64c742f0931a3058a1c2a48116307
Noel Caponpon <caponponnoel_bsit@plmun.edu.ph>
2025-02-10 01:44:21 +0800

    Submitting Python Week 02 - Session 01 - Exercise 03

---
+++ exercise_03.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+c = 3
+
+result1 = a + b * c
+print("Result of a + b * c:", result1)
+
+result2 = (a + b) * c
+print("Result of (a + b) * c:", result2)
+
+result3 = a - b
+print("Result of a - b:", result3)
+
+result4 = a / b
+result5 = a // b
+print("Result of a / b:", result4)
+print("Result of a // b (floor division):", result5)
